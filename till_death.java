816799c7afad9b9298fcc4d41007e91109837234
Atanu Debnath <atanuc2005@gmail.com>
2026-02-11 19:54:11 +0530

    feat(limit): add limit for special moves

--- till_death.java
+++ till_death.java
@@ -2,12 +2,35 @@ import java.nio.channels.Pipe.SourceChannel;
 import java.util.*;
 
 public class till_death {
-    public static void showMenu() {
+    public static void showRules() {
+        System.out.println("Welcome to Till Death! A 1v1 battle simulation");
+        System.out.println();
+        System.out.println("Rules:");
+        System.out.println("1) Both starts with 1000 HP.");
+        System.out.println("Toss will dicide first move");
+        System.out.println("2) You have 3 differnt attack modes, choose wisely as per limits!");
+        System.out.println("3) You have an option to heal by 300 by only ONCE!");
+        System.out.println("First player to hit 0 HP will loose lol :)");
+    }
+    public static void showMenu(int slayLeft, int abilityLeft, int healLeft) {
         System.out.println("Choose button:");
         System.out.println("/1 Basic");
-        System.out.println("/2 slay");
-        System.out.println("/3 Ability");            
-        System.out.println("/4 heal");
+        System.out.println("/2 slay" + "(Left: " + slayLeft + ")");
+        System.out.println("/3 Ability" + "(Left: " + abilityLeft + ")");            
+        System.out.println("/4 heal" + "(Left: " + healLeft + ")");
+    }
+    public static boolean LimitCheck(int move, int healLeft, int slayLeft, int abilityLeft) {
+        if(move == 2 && slayLeft == 0) {
+            System.out.println("Slay already used, choose another move");
+            return false;
+        } else if(move == 3 && abilityLeft == 0) {
+            System.out.println("Ability already used, choose another move");
+            return false;
+        } else if(move == 4 && healLeft == 0) {
+            System.out.println("Heal already used, choose another move");
+            return false;
+        }
+        return true;
     }
     public static int playerTurn(int move, int botHealth) {
         if(move == 1) {
@@ -21,39 +44,43 @@ public class till_death {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Till Death! A 1v1 battle simulation");
-        System.out.println();
-        System.out.println("Rules:");
-        System.out.println("1) Both starts with 1000 HP.");
-        System.out.println("Toss will dicide first move");
-        System.out.println("2) You have 3 differnt attack modes, choose wisely as per limits!");
-        System.out.println("3) You have an option to heal by 300 by only ONCE!");
-        System.out.println("First player to hit 0 HP will loose lol :)");
-
-        //Player name input
-        System.out.print("Enter your name: ");
+        showRules(); //Showing rules
+        System.out.print("Enter your name: "); //Player name input
         String name = sc.nextLine();
         System.out.println();
         int myHealth = 1000;
         int botHealth = 1000;
 
-        //Game Loop
         boolean turn = true;
+        int healLeft = 1; //To track if heal is used
+        int slayLeft = 2; //To track if slay is used
+        int abilityLeft = 1; //To track if ability is used
+
+        //Game loop
         while(true) {
             if(turn) { //Player's turn
                 System.out.println("_________________________________");
                 System.out.println(name + "'s Health: " + myHealth);
                 System.out.println("Bot's Health: " + botHealth);
                 System.out.println("_________________________________");
-                showMenu(); //Showing attack options
+                showMenu(slayLeft, abilityLeft, healLeft); //Showing attack options
                 int move = sc.nextInt(); //Player move input
                 if(move < 1 || move > 4) {
                     System.out.println("Invalid move, try again");
                     continue; //ask again
                 }
-                if(move == 4) {
+                if(!LimitCheck(move, healLeft, slayLeft, abilityLeft)) {
+                    continue; //ask again
+                }
+                if(move == 2) {
+                    slayLeft--;
+                } else if(move == 3) {
+                    abilityLeft--;
+                } else if(move == 4 && healLeft == 1) {
                     myHealth = Math.min(myHealth + 300, 1000); //Healing, but not exceeding 1000
-                } else {
+                    healLeft = 0; //Heal used
+                }
+                if(move != 4) { //If not healing, then attack
                     botHealth = playerTurn(move, botHealth);
                 }
             }else { //Bot's turn
